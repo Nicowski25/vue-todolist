35a439fd269ad19e255a2e967ec89cfc9e2374ce
Nicowski25 <nicola.faedo0610@gmail.com>
2023-04-05 11:38:10 +0200

    fixed input and css style

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const { createApp } = Vue
 createApp({
 data() {
     return {
+        newTask: '',
         tasks: [
             {
                 text: 'learn HTML',
@@ -16,12 +17,18 @@ data() {
                 text: 'learn JS',
                 done: true
             }
-        ]
+        ],
     }
 },
 methods: {
     removeTask(index) {
         this.tasks.splice(index, 1);
+    },
+    addTask() {
+        if (this.newTask.length > 0) {
+        this.tasks.push({text: this.newTask.charAt(0).toUpperCase()+this.newTask.slice(1), done: false});
+        this.newTask = '';
+        }
     }
 }
 }).mount('#app')
